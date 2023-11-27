f9361f816a89bc0502254512fc1ac88315ee66a5
Sanli senac <sanlibayer@gmail.com>
2023-11-27 20:53:27 -0300

    Commit avaliação e index

---
+++ avaliacao.js
@@ -0,0 +1,10 @@
+function showModal() {
+    let estrelaUM = document.querySelector("#estrelaUM");
+    let estrelaDOIS = document.querySelector("#estrelaDOIS");
+    let estrelaTRES = document.querySelector("#estrelaTRES");
+    let estrelaQUATRO = document.querySelector("#estrelaQUATRO");
+    let estrelaCINCO = document.querySelector("#estrelaCINCO");
+    estrelaUM.classList.add("opacity: 0 !important;");
+  
+
+  }
