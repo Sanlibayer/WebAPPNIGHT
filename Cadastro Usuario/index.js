138603944b4424edbc8d6f6db273e2f4a9b15a45
RicardoMandarino <146490786+RicardoMandarino@users.noreply.github.com>
2023-11-29 20:10:59 -0300

    mudando de lugar

---
+++ Cadastro Usuario/index.js
@@ -0,0 +1,18 @@
+$(() => {
+
+    $("#btn-cadastro-usario").click(() => {
+        const values = {
+            nome: $("#nome")[0].value,
+            sobrenome: $("#sobrenome")[0].value,
+            cpf:
+        
+    }
+})
+
+$("#btn-add").click(() => {
+    const values = {
+        imagem: "produto2.png",
+        nome: $("#nome")[0].value,
+        preco: $("#preco")[0].value,
+        parcelas: 10
+    }
