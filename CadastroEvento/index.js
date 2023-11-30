bfd1edd576bf4d6575c4777675f8f6db806896de
Sanli senac <sanlibayer@gmail.com>
2023-11-29 21:30:52 -0300

    submit e validação com JS

--- CadastroEvento/index.js
+++ CadastroEvento/index.js
@@ -1,2 +1,36 @@
+$(() => {
+    $("#btn-criar-evento").click(() => {
+        const eventos = {
+            nome: $("#nome-evento-input")[0].value,
+            data: $("#data-evento-input")[0].value,
+            descricao: $("#descricao-evento-input")[0].value,
+        }
+        if (!eventos.nome) {
+            alert("Nome do evento não informado!");
+            $("#nome-evento-input").addClass("invalid");
+            return;
+        }
+        $("#nome-evento-input").removeClass("invalid");
+
+        if (!eventos.data) {
+            alert("Data do evento não informado!");
+            $("#data-evento-input").addClass("invalid");
+            return;
+        }
+
+        $("#data-evento-input").removeClass("invalid");
+
+        if (!eventos.descricao) {
+            alert("Informe alguma descrição para o evento!");
+            $("#descricao-evento-input").addClass("invalid");
+            return;
+        }
+        
+        $("#descricao-evento-input").removeClass("invalid");
+
+        console.log(eventos);
+
+    })
+})
 
 
--- avaliacao.js
+++ avaliacao.js
@@ -1,10 +1,34 @@
-function showModal() {
-    let estrelaUM = document.querySelector("#estrelaUM");
-    let estrelaDOIS = document.querySelector("#estrelaDOIS");
-    let estrelaTRES = document.querySelector("#estrelaTRES");
-    let estrelaQUATRO = document.querySelector("#estrelaQUATRO");
-    let estrelaCINCO = document.querySelector("#estrelaCINCO");
-    estrelaUM.classList.add("opacity: 0 !important;");
-  
+$(() => {
+  let selecionado = 0;
 
+  $(".estrelaa").click((e) => {
+    selecionado = $(e.currentTarget).data('number');
+    $(".estrelaa").each((i, el) => {      
+      if ($(el).data('number') <= selecionado) {
+        $(el).addClass('selected');
+      } else {
+        $(el).removeClass('selected');
+      }
+    })
+
+  })
+
+
+  $("#btn-enviar").click(() => {
+    const avaliacao = {
+      nota : selecionado,
+      opiniao: $("#opiniao")[0].value
+
+    }
+    if (!avaliacao.nota) {
+      alert("Nota não informada!");
+      $(".estrelaa").addClass("invalid");
+      return;
   }
+  $(".estrelaa").removeClass("invalid");
+
+  console.log(avaliacao);
+
+  })
+
+})
